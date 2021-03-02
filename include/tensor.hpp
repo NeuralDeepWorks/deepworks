9e30e88fce2c1bbbf5ff19f1269f20758b0eafc8
Aleksandr Pertovsky <aleksandr.pertovsky@intel.com>
2021-03-02 21:35:44 +0300

    Add Tensor class (#3)
    * Add Tensor class

---
+++ include/tensor.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+
+namespace deepworks {
+
+using Shape = std::vector<int>;
+using Strides = std::vector<size_t>;
+
+class Tensor {
+public:
+    using Type = float;
+
+    Tensor();
+    explicit Tensor(const Shape &shape);
+
+    void copyTo(Tensor tensor);
+    Type *data();
+    size_t total() const;
+    void allocate(const Shape& shape);
+    bool empty() const;
+    const Strides &strides() const;
+    const Shape &shape() const;
+
+    ~Tensor() = default;
+
+private:
+    class Descriptor;
+    std::shared_ptr<Descriptor> m_descriptor;
+};
+
+struct Tensor::Descriptor {
+    Descriptor() = default;
+    explicit Descriptor(const Shape& shape);
+
+    void copyTo(Tensor::Descriptor& descriptor);
+    void allocate(const Shape& shape);
+    void calculateStrides(const Shape& shape);
+
+    ~Descriptor();
+
+    Strides m_strides;
+    Shape m_shape;
+    Type *m_data{nullptr};
+    size_t m_total{0ul};
+};
+
+} // namespace deepworks
