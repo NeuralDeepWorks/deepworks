dd4378f274127abe6da3c9174feb85ab3abc3144
TolyaTalamanov <anatoliy.talamanov@intel.com>
2021-04-01 14:32:54 +0300

    Remove const from data()

--- include/deepworks/tensor.hpp
+++ include/deepworks/tensor.hpp
@@ -17,8 +17,7 @@ public:
     Tensor(const Shape& shape, float* data);
     explicit Tensor(const Shape& shape);
 
-    Type*           data();
-    const Type*     data()    const;
+    Type*           data()    const;
     size_t          total()   const;
     bool            empty()   const;
     const  Strides& strides() const;
