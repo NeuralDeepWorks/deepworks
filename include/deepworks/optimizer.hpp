1083171d8cf6106206f9d30a0224cccd1b0b10e7
Fedor <fed-rodionov22@yandex.ru>
2021-04-25 14:39:48 +0300

    [optimizer] Momentum (#93)
    * init momentum

--- include/deepworks/optimizer.hpp
+++ include/deepworks/optimizer.hpp
@@ -21,5 +21,22 @@ private:
     Parameters& m_params;
 };
 
+class SGDMomentum {
+public:
+    explicit SGDMomentum(Parameters& params, float lr, float gamma = 0.9f);
+
+    void step();
+
+    float get_lr() const;
+
+    void set_lr(float lr);
+
+private:
+    float m_lr;
+    float gamma;
+    Parameters& m_params;
+    std::vector<Tensor> velocities;
+};
+
 } // namespace loss
 } // namespace deepworks
