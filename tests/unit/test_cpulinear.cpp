5f5f9c223dd6511fdf4f2e9440ddfcd50ff6e40e
Anatoliy Talamanov <anatoliy.talamanov@intel.com>
2021-04-26 23:35:18 +0300

    Update test_cpulinear.cpp

--- tests/unit/test_cpulinear.cpp
+++ tests/unit/test_cpulinear.cpp
@@ -76,7 +76,7 @@ struct CPULinearAfterConvolutionModelTest : public ::testing::Test {
               model(buildModel()) {
         model.compile();
 
-	    W_conv  = model.getLayer("conv1").params().at("weight").data();
+	W_conv  = model.getLayer("conv1").params().at("weight").data();
         b_conv  = model.getLayer("conv1").params().at("bias").data();
         W       = model.getLayer("linear2").params().at("weight").data();
         b       = model.getLayer("linear2").params().at("bias").data();
